96df5e4ec280041f26b802e5406d9e2d122a234b
Victoria Misch <vickie.misch@gmail.com>
2015-06-09 13:46:56 -0400

    upload files

---
+++ js/main.js
@@ -0,0 +1,64 @@
+"use strict";
+
+$(document).click(function(){
+    $('.parallax-window').parallax({imageSrc: 'img/jsflip_01.jpg'});
+
+    //$('#menu').accordion();
+    
+    //$('#video-game-characters').sortable();
+
+    //$('#characters').selectable();
+
+    //$('img').animate({top: '+=100px'}, 1000);  
+});
+
+$(document).click(function(){
+   $( "#tabs" ).tabs();
+    console.log("is this working"); 
+});
+
+
+
+/*$("#button1").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#about").offset().top
+    }, 1000);
+});
+
+$("#button2").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#services").offset().top
+    }, 1000);
+});
+
+
+
+$("#button3").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#contact").offset().top
+    }, 1000);
+});*/
+
+/*$('a[href^="#"]').on('click', function(event) {
+    var target = $(this.href);
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: target.offset().top
+        }, 1000);
+    }
+});*/
+
+/*
+$('html, body').animate({
+    scrollTop: $("#contact").offset().top
+}, 1000);*/
+
+
+//$('#button2').click(function() {
+  //  ('html, body').scrollTo('#services');
+//});
+
+
+    
+    
